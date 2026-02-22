2574577eb476cfe5950db31982dd77fe0830ee27
Alif Rahmadian <alifrah78@yahoo.com>
2026-02-22 23:12:45 +0700

    feat: add menu choice view

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import mysql.connector
+
+def main():
+    print("Selamat Datang di Maxi Bookstore\n\n")
+    print("1. Tampilkan Data")
+    print("2. Tambah Data Baru")
+    print("3. Tampilkan Rata-Rata")
+    print("4. Tampilkan Visualisasi Data")
+    print("5. Keluar")
+
+
+if __name__ == 'main':
+    main()
