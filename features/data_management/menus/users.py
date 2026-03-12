fcf33b0eb77d6f323eabfbe6e4999fbac8883d58
Alif Rahmadian <alifrah78@yahoo.com>
2026-03-12 22:03:10 +0700

    fix: remove unecessary menus

--- features/data_management/menus/users.py
+++ features/data_management/menus/users.py
@@ -4,16 +4,13 @@ def menu(connection):
     while True:
         print("\n === DATA USER === ")
         print("1. Tambah data user")
-        print("2. Ubah data user")
-        print("3. Kembali ke menu sebelumnya")
+        print("2. Kembali ke menu sebelumnya")
 
-        choice = int(input("Masukkan pilihan anda (1-3): "))
+        choice = int(input("Masukkan pilihan anda (1-2): "))
 
         if choice == 1:
             add_user(connection)
         elif choice == 2:
-            pass
-        elif choice == 3:
             break
         else:
             print("Pilihan tidak valid")
